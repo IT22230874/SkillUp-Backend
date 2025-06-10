f2c42366861199a3753b90150e7023f7d17679e7
savindu-c <it22230874@my.sliit.lk>
2025-06-10 13:17:11 +0530

    port update

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/courses', courseRoutes);
 const studentRoutes = require('./routes/studentRoutes');
 app.use('/api/student', studentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
